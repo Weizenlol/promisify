a4ee3d5e6bd44c7c42afe2c914a35bd1b93cb3ab
weizenlol <winliveweiz@gmail.com>
2015-10-28 17:59:07 +0300

    added basic methods and tests

--- index.js
+++ index.js
@@ -1,3 +1,56 @@
+"use strict";
+
+var Promisify = {};
+
 /**
- * Created by zander on 28.10.2015.
+ * Promise chain.
+ * Stops when some promise resolve.
+ * EXAMPLE:
+ * var promises = [promiseAPIMethod1, promiseAPIMethod2, promiseAPIMethod3];
+ * Promisify.firstInChain(promises).then(function(finalResult){
+ *      //do some with result
+ * }).catch(function(finalError){
+ *      //handle error
+ * });
+ * @param {(function(...)Promise)[]} promiseMethods
+ * @param {function|undefined} errHandler
+ * @returns Promise
  */
+Promisify.firstInChain = function (promiseMethods, errHandler) {
+    return promiseList.reduce(function(current, next){
+        return current.catch(function(err){
+            (typeof errHandler === "function") && errHandler(err);
+            return next();
+        }).then(function(data){
+            return data;
+        });
+    }, Promise.reject("Initial error"));
+};
+
+/**
+ * Promise chain
+ * @param promiseMethods
+ * @returns Promise
+ */
+Promisify.chain = function (promiseMethods) {
+    return promiseMethods.reduce(function (current, next) {
+        return current.then(next);
+    }, Promise.resolve());
+};
+
+/**
+ * Promise chain, TURN DOWN FOR WHAT
+ * @param promiseMethods
+ * @returns Promise
+ */
+Promisify.chainForce = function (promiseMethods) {
+    return promiseMethods.reduce(function (current, next) {
+        return current.catch(function (err) {
+            return next(err);
+        }).then(function () {
+            return next.apply(next, arguments);
+        });
+    }, Promise.resolve());
+};
+
+module.exports = Promisify;
---
+++ tests/test.js
@@ -0,0 +1,85 @@
+/**
+ * Created by zander on 28.10.2015.
+ */
+var test = require('tape');
+var promisify = require('./../index');
+
+var promiseApi = {};
+
+promiseApi.reject1 = function (arg) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function(){
+            reject("reject1");
+        }, 1000);
+    });
+};
+
+promiseApi.reject2 = function (arg) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function(){
+            reject("reject2");
+        }, 1000);
+    });
+};
+
+promiseApi.dataChain = function (arg) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function(){
+            resolve(arg+1);
+        }, 1000);
+    });
+};
+
+promiseApi.dataChainWithAck = function (ackCallback) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function(){
+            ackCallback();
+            resolve("ok");
+        }, 1000);
+    });
+};
+
+promiseApi.staticData = function (arg) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function(){
+            resolve("staticData");
+        }, 1000);
+    });
+};
+
+test('chain resolve test', function (t) {
+    t.plan(1);
+    var promiseFabricChain = [promiseApi.dataChain.bind(promiseApi, 0), promiseApi.dataChain, promiseApi.dataChain, promiseApi.dataChain];
+    var finalPromise = promisify.chain(promiseFabricChain);
+    finalPromise.then(function (data) {
+        t.equal(4, data);
+    }).catch(function(){
+        t.fail("No rejection allowed for this test.");
+    });
+});
+
+test('chain resolve-reject test', function (t) {
+    t.plan(1);
+    var promise = promisify.chain([promiseApi.dataChain.bind(promiseApi, 0), promiseApi.dataChain, promiseApi.reject1, promiseApi.dataChain]);
+    promise.then(function (data) {
+        t.fail("No resolve allowed for this test.");
+    }).catch(function(err){
+        t.equal("reject1", err);
+    });
+});
+
+test('force chain test', function (t) {
+    t.plan(5);
+    var promiseFabricChain = [
+        promiseApi.dataChainWithAck.bind(promiseApi, t.ok.bind(t, true, "resolve 1")),
+        promiseApi.dataChainWithAck.bind(promiseApi, t.ok.bind(t, true, "resolve 2")),
+        promiseApi.dataChainWithAck.bind(promiseApi, t.ok.bind(t, true, "resolve 3")),
+        promiseApi.dataChainWithAck.bind(promiseApi, t.ok.bind(t, true, "resolve 4"))
+    ];
+    var finalPromise = promisify.chainForce(promiseFabricChain);
+    finalPromise.then(function (data) {
+        t.ok(true, "final resolve");
+    }).catch(function(){
+        t.fail("No rejection allowed for this test.");
+    });
+});
